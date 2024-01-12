d9c06692c780e23034053694a6ec45570693fd69
ddasnd <139565074+ddasnd@users.noreply.github.com>
2024-01-12 17:43:57 +0400

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,20 @@
+let songs = [
+  {
+      name: "Dil DIl Pakistan - Junaid Jamshed",
+      artist: "Junaid Jamshed",
+      img: "DIl-DIl",
+      audio: "Dil Dil Pakistan by Junaid Jamshed"
+    },
+  {
+      name: "King Shit - Shubh",
+      artist: "Shubh",
+      img: "Shubh",
+      audio: "King Shit - Shubh"
+    },
+  {
+      name: "Safety Off - Shubh",
+      artist: "Shubh",
+      img: "img2",
+      audio: "Safety Off"
+   },
+  ]
